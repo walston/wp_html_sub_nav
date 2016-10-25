99c1f9c43ee6e003d0e6a3ac8b0ea0ec3cc4f24c
walston <nathanielwalston@gmail.com>
2016-10-25 10:53:21 -0700

    don't waste time wrapping nothing

--- theme-functions.php
+++ theme-functions.php
@@ -1,12 +1,14 @@
 <?php
 
 if ( class_exists( 'Walker_Nav_Menu' ) ) {
-  
+
   class GoCheck_Site_Nav_Walker extends Walker_Nav_Menu {
     public function end_el( &$output, $item, $depth = 0, $args = array() ) {
-      $output .= '<div class="sub-menu"><div class="container"><div class="row">';
-      $output .= $item->post_content;
-      $output .= '</div></div></div>';
+      if ($item->post_content && $item->post_content !== " "){
+        $output .= '<div class="sub-menu"><div class="container"><div class="row">';
+        $output .= $item->post_content;
+        $output .= '</div></div></div>';
+      }
       $output .= "</li>\n";
     }
 
