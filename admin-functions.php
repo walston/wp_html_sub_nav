bda312cd6f96f7525b07b328a61401fe5a2cc8ae
walston <nathanielwalston@gmail.com>
2016-10-27 09:34:26 -0700

    Add some documentation

--- admin-functions.php
+++ admin-functions.php
@@ -17,11 +17,43 @@ function admin_walker($walker)
 
 include_once(dirname(__FILE__) . "/class_HTML_Sub_Menu_Controller.php");
 
-new HTML_Sub_Menu\Controller(array(
-  'html-sub-menu' => 'HTML Sub-Menu'
-));
+/**
+ *  Construct a new navigation menu item, and generate a corresponding action
+ *  of format "menu-item-{$slug}-render" for rendering html
+ *
+ *  @since 1.0.0
+ *
+ *  @argument array in format $slug => $label
+ *
+ */
+new HTML_Sub_Menu\Controller(array('html-sub-menu' => 'HTML Sub-Menu'));
 
+/**
+ *  Automatically generated action for each new form-field
+ *
+ *  @since 1.0.0
+ *
+ *  @argument string  $action-slug Generated as "menu-item-{$slug}-render"
+ *  @argument string  $callable    Function name of renderer
+ *  @argument integer $priority    Lower = earlier (should only be 1 render callable per menu-item)
+ *  @argument integer $arguments   should always be 6, see documentation below
+ *
+ */
 add_action('menu-item-html-sub-menu-render', 'html_sub_menu_render', 1, 6);
+
+/**
+ *  Rendering function for menu-item-html-sub-menu-render
+ *
+ *  @since 1.0.0
+ *
+ *  @param string $label  Label to display above control in HTML
+ *  @param string $key    Post_Meta key, used to find in wp_postmeta table
+ *  @param string $id     REQUIRED HTML id tag
+ *  @param string $name   REQUIRED form input name, used for POST submission
+ *  @param string $value  REQUIRED the value of this Post_Meta
+ *  @param string $class  REQUIRED wordpress builtins for paragraph wrapper
+ *
+ */
 function html_sub_menu_render($label, $key, $id, $name, $value, $class)
 {
   ?>
