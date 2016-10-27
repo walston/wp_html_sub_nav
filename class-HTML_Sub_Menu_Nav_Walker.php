e9308a989d7740f7b5a604b8f964fadadf395d69
walston <nathanielwalston@gmail.com>
2016-10-27 14:57:41 -0700

    front end walker needs to fetch post data now

--- class-HTML_Sub_Menu_Nav_Walker.php
+++ class-HTML_Sub_Menu_Nav_Walker.php
@@ -5,11 +5,13 @@ class Nav_Walker extends \Walker_Nav_Menu
 {
   public function end_el(&$output, $item, $depth = 0, $args = array())
   {
-    if (get_post_meta($item->ID, 'menu-item-html-sub-menu', true)){
+    $submenuID = get_post_meta($item->ID, 'menu-item-html-sub-menu', true);
+
+    if ($submenuID){
       $output .= '<div class="sub-menu">';
       $output .= '<div class="container">';
       $output .= '<div class="row">';
-      $output .= get_post_meta($item->ID, 'menu-item-html-sub-menu', true);
+      $output .= get_post($submenuID)->post_content;
       $output .= '</div></div></div>';
     }
     parent::end_el($output, $item, $depth, $args);
