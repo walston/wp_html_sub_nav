3103e3dfa7b89b913379649cf7a8d343fd805ad6
walston <nathanielwalston@gmail.com>
2016-10-27 09:58:21 -0700

    using Thickbox to modalize

--- admin-functions.php
+++ admin-functions.php
@@ -56,14 +56,23 @@ add_action('menu-item-html-sub-menu-render', 'html_sub_menu_render', 1, 6);
  */
 function html_sub_menu_render($label, $key, $id, $name, $value, $class)
 {
+  $wrapperId = $id . '-wrapper';
+  add_thickbox();
   ?>
   <p class="description description-wide <?php echo esc_attr($class) ?>">
-    <label for="<?php echo esc_attr($id); ?>">
+    <label for="<?php echo esc_attr($wrapperId); ?>">
       <?php echo esc_html($label); ?><br />
-      <div id="<?php echo esc_attr($id) . '-wrapper'; ?>" class="hidden">
-        <textarea id="<?php echo esc_attr($id); ?>" name="<?php echo esc_attr($name) ?>"><?php echo esc_textarea($value); ?></textarea>
-      </div><!-- .hidden -->
-      <input type="button" class="button-secondary <?php echo esc_attr($id); ?>" onclick="jQuery('#<?php echo esc_attr($id) . '-wrapper'; ?>').toggleClass('hidden');" value="Edit HTML Sub-Menu" />
+      <fieldset id="<?php echo esc_attr($wrapperId); ?>" class="hidden">
+        <p>
+          <label for="<?php echo esc_attr($id); ?>">
+            <?php echo esc_html($label); ?>
+            <textarea id="<?php echo esc_attr($id); ?>" name="<?php echo esc_attr($name) ?>" class="widefat"><?php echo esc_textarea($value); ?></textarea>
+          </label>
+        </p>
+      </fieldset><!-- .hidden -->
+      <a href="TB_inline?width=600&height=550&inlineId=<?php echo esc_attr($wrapperId); ?>" class="thickbox">
+        <input type="button" class="button-secondary <?php echo esc_attr($id); ?>" value="Edit HTML Sub-Menu" />
+      </a>
     </label>
   </p>
   <?php
